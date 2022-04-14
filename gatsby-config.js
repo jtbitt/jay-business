9d2d07a2e1e9fc3b299e83d57d139b9ea7298ffd
Jay Bittner <jtbitt@gmail.com>
2022-04-13 17:37:02 -0700

    Setting up gatsby boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    siteMetadata: {
-      title: `jay-business`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: `jay-business`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
