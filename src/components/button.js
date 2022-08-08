2f26b0b4b81cdfc0c18a141e0a74b0fd6224b929
Jay Bittner <jtbitt@gmail.com>
2022-08-08 15:09:13 -0500

    Fixed custom button margin to account for translate spacing

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const Button = ({ className, name }) => {
   return (
     <button
-      className={`rounded h-9 w-36 md:h-12 md:w-40 bg-white ml-1 ${className}`}
+      className={`rounded h-9 w-36 md:h-12 md:w-40 bg-white ml-1 mt-3 ${className}`}
     >
       <span className="h-full flex justify-center items-center border-2 rounded border-white -translate-x-1 -translate-y-1 text-white text-sm md:text-base font-medium tracking-widest bg-gray-500">
         {name}
