b58ccebb9e1ff4beb4b859e05efd468398b961c3
Jay Bittner <jtbitt@gmail.com>
2022-09-07 22:21:18 -0500

    Adjusted heading accessibility for projects

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -19,15 +19,25 @@ export const Project = ({
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
       <GatsbyImage image={img} alt={alt} />
       <div className="space-y-6 md:space-y-8">
-        <Heading type="h2">{title}</Heading>
+        <Heading type="h3" size="text-4xl md:text-5.5xl 2xl:text-6xl">
+          {title}
+        </Heading>
         <div className="space-y-2">
-          <Heading type="h5" basicFont="true">
+          <Heading
+            type="h4"
+            size="text-xl md:text-2xl 2xl:text-3xl"
+            basicFont="true"
+          >
             {subtitle}
           </Heading>
           <Description>{description}</Description>
         </div>
         <div className="space-y-2">
-          <Heading type="h5" basicFont="true">
+          <Heading
+            type="h4"
+            size="text-xl md:text-2xl 2xl:text-3xl"
+            basicFont="true"
+          >
             Development Tools
           </Heading>
           <Description>{tools}</Description>
