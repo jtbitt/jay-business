c0cab10334b04368ba4a3f67fc2594603c749dba
Jay Bittner <jtbitt@gmail.com>
2022-09-09 21:50:05 -0500

    Done setting up services page

--- src/components/services/Faq.jsx
+++ src/components/services/Faq.jsx
@@ -5,7 +5,7 @@ import { Heading, Description } from "@components";
 export const Faq = ({ question, answer }) => {
   return (
     <div className="flex flex-col">
-      <Heading type="h5">{question}</Heading>
+      <Heading type="h3" size="text-lg md:text-xl 2xl:text-2xl" className="mb-4">{question}</Heading>
       <Description>{answer}</Description>
     </div>
   );
--- src/pages/services.js
+++ src/pages/services.js
@@ -14,31 +14,32 @@ import faqs from "@content/faqs.json";
 const ServicesPage = () => {
   return (
     <Layout>
-      <div className="space-y-8">
+      <div>
         <Heading
           type="h1"
           size="text-4xl md:text-5.5xl 2xl:text-6xl"
-          className="text-center"
+          className="text-center mb-8"
         >
           Accelerate your projects
         </Heading>
-        <Description className="md:px-32 lg:px-36 xl:px-40 text-center">
+        <Description className="md:px-32 lg:px-36 xl:px-40 text-center mb-8">
           Stop searching endlessly for a solid contributor. Let's focus on
           what's important and start building the projects that you care about
           today.
         </Description>
-        <section className="grid grid-cols-1 xl:grid-cols-3 gap-10 md:px-32 lg:px-40 xl:px-0">
+        <section
+          className="grid grid-cols-1 xl:grid-cols-3 gap-10 md:px-32 lg:px-40 xl:px-0 mb-20"
+          aria-label="Select Product"
+        >
           {services.map((service, key) => (
             <ServiceOption {...service} key={key} />
           ))}
         </section>
-        <section>
-          <Heading type="h1">How it works</Heading>
-          <div className="grid grid-cols-1 md:grid-cols-3"></div>
-        </section>
-        <section>
-          <Heading type="h1">FAQ</Heading>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <section aria-label="Frequently Asked Questions">
+          <Heading type="h2" className="mb-8 md:mb-10">
+            FAQ
+          </Heading>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
             {faqs.map((faq, key) => (
               <Faq {...faq} key={key} />
             ))}
