8da3defde07659bd29c474772a48b7558a0ce516
Jay Bittner <jtbitt@gmail.com>
2022-09-16 20:09:06 -0500

    Set up connect section

--- src/components/home/Connect.jsx
+++ src/components/home/Connect.jsx
@@ -1,20 +1,53 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
+import { PopupButton } from "react-calendly";
 
-import { Heading, Description, Button } from "@components";
+import { Heading, Description } from "@components";
+import { GithubIcon, LinkedinIcon } from "@icons";
 
 export const Connect = ({ images }) => {
+  const [portal, setPortal] = useState(null);
+
+  useEffect(() => {
+    const root = document.getElementById('portal');
+    setPortal(root);
+  }, []);
+
   return (
     <section className="space-y-12 pt-36" id="connect" aria-label="Connect">
-      <Heading type="h2">Let's Connect</Heading>
+      <Heading type="h2">Connect</Heading>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16">
         <GatsbyImage
           image={images.connect}
           alt="A laptop computer open on a desk with a Front End Developer services website open"
         />
-        <div className="text-center md:text-left space-y-12">
-          <Description>If you would like to work together</Description>
-          <Button>GET IN TOUCH</Button>
+        <div className="text-left space-y-8">
+          <Heading type="h3" basicFont={true}>Let's build something <br /> amazing together</Heading>
+          <Description>
+            Start by scheduling a&nbsp;
+            <PopupButton
+              url="https://calendly.com/jaybittner/15min"
+              rootElement={portal}
+              className="text-base md:text-lg 2xl:text-xl font-light tracking-widest text-amber-500"
+              text="Meeting"
+            />
+            &nbsp;/&nbsp;
+            <Link className="text-amber-500" to="">
+              Email Me
+            </Link>
+          </Description>
+          <Description>OR</Description>
+          <Description>
+            Take a look at my&nbsp;
+            <Link className="text-amber-500" to="/services">
+              Services
+            </Link>
+          </Description>
+          <div className="flex flex-row gap-8 h-16">
+            <GithubIcon className="h-8 w-8 2xl:h-12 2xl:w-12" />
+            <LinkedinIcon className="h-8 w-8 2xl:h-12 2xl:w-12" />
+          </div>
         </div>
       </div>
     </section>
