4805c3c64ffea20498a061bc72ab75ba5433e014
Jay Bittner <jtbitt@gmail.com>
2022-09-26 01:17:18 -0500

    One more try

--- src/components/shared/RootWrapper.jsx
+++ src/components/shared/RootWrapper.jsx
@@ -4,7 +4,11 @@ import { Script } from "gatsby";
 export const RootWrapper = ({ children }) => {
   return (
     <>
-      <Script id="hotjar" strategy="off-main-thread">
+      <Script
+        id="hotjar"
+        strategy="off-main-thread"
+        forward={[`hj`, `_hjSettings`]}
+      >
         {`(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:3170670,hjsv:6};
@@ -12,14 +16,15 @@ export const RootWrapper = ({ children }) => {
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`}
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+        `}
       </Script>
-      <Script
+      {/* <Script
         id="sentry"
         strategy="off-main-thread"
         src="https://js.sentry-cdn.com/37a0156551a747eaa3ba96a5060c886e.min.js"
         crossOrigin="anonymous"
-      ></Script>
+      ></Script> */}
       {children}
     </>
   );
