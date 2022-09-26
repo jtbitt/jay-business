059a88b66f351c8258a4681d91a02e6cd5c1bbdc
Jay Bittner <jtbitt@gmail.com>
2022-09-26 16:44:31 -0500

    Added a new test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,34 @@ export const wrapRootElement = ({ element }) => {
   return <RootWrapper>{element}</RootWrapper>;
 };
 
-export const onRenderBody = ({ setHtmlAttributes, setPostBodyComponents }) => {
+export const onRenderBody = ({
+  setHtmlAttributes,
+  setHeadComponents,
+  setPostBodyComponents,
+}) => {
   setHtmlAttributes({ lang: "en" });
+  setHeadComponents([
+    <script
+      key="partytown-vanilla-config"
+      dangerouslySetInnerHTML={{
+        __html: `partytown = {
+          resolveUrl(url, location) {
+            console.log(url, location);
+            switch (url.hostname) {
+              case 'static.hotjar.com': {
+                return
+                }
+                case 'vars.hotjar.com': {
+                return
+                }
+                case 'script.hotjar.com': {
+                  return
+                }
+            }
+          }
+        }`,
+      }}
+    />,
+  ]);
   setPostBodyComponents([<div id="portal" key="portal"></div>]);
 };
--- src/components/shared/RootWrapper.jsx
+++ src/components/shared/RootWrapper.jsx
@@ -2,31 +2,26 @@ import React from "react";
 import { Script } from "gatsby";
 
 export const RootWrapper = ({ children }) => {
-  const [sentryLoaded, setSentryLoaded] = useState(false);
-
   return (
     <>
       <Script
         id="sentry"
         src="https://js.sentry-cdn.com/37a0156551a747eaa3ba96a5060c886e.min.js"
         crossOrigin="anonymous"
-        onLoad={() => setSentryLoaded(true)}
       ></Script>
-      {sentryLoaded && (
-        <Script id="hotjar">
-          {`
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3170670,hjsv:6};
-              h.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-        </Script>
-      )}
+      <Script id="hotjar" strategy="off-main-thread" forward={[`hj`]}>
+        {`
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3170670,hjsv:6};
+            h.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+      </Script>
       {children}
     </>
   );
