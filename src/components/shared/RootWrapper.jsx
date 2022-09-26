c097b91867f4cae3112b9c94f459fb9b51ed3a57
Jay Bittner <jtbitt@gmail.com>
2022-09-26 14:16:27 -0500

    Another test

--- src/components/shared/RootWrapper.jsx
+++ src/components/shared/RootWrapper.jsx
@@ -6,28 +6,22 @@ export const RootWrapper = ({ children }) => {
     <>
       <Script
         id="hotjar"
+        src="./hotjar.js"
         strategy="off-main-thread"
-        crossOrigin="anonymous"
-        src="https://static.hotjar.com/c/hotjar-3170670.js?sv=6"
-        forward={[`hj`, `_hjSettings`, `hjSiteSettings`, `hjBootstrap`]}
+        forward={[`hj`, `_hjSettings`]}
       >
         {`
           (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3170670,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            window.hj=window.hj||function(){(window.hj.q=windows.hj.q||[]).push(arguments)};
+            window._hjSettings={hjid:3170670,hjsv:6};
+          })(window);
         `}
       </Script>
-      {/* <Script
+      <Script
         id="sentry"
-        strategy="off-main-thread"
         src="https://js.sentry-cdn.com/37a0156551a747eaa3ba96a5060c886e.min.js"
         crossOrigin="anonymous"
-      ></Script> */}
+      ></Script>
       {children}
     </>
   );
