c08c7bffeb4e537bcfb95e66d108ff2a2535d29e
Jay Bittner <jtbitt@gmail.com>
2022-09-27 11:01:42 -0500

    Disabling off thread stuff for now

--- src/components/shared/RootWrapper.jsx
+++ src/components/shared/RootWrapper.jsx
@@ -13,8 +13,9 @@ export const RootWrapper = ({ children }) => {
         id="plausible"
         data-domain="jaybittner.com"
         src="https://plausible.io/js/plausible.js"
+        crossOrigin="anonymous"
       ></Script>
-      <Script id="hotjar-init" strategy="off-main-thread">
+      {/* <Script id="hotjar-init" strategy="off-main-thread">
         {`
           window.hj=window.hj||function(){(window.hj.q=window.hj.q||[]).push(arguments)};
           window._hjSettings={hjid:3170670,hjsv:6};
@@ -25,7 +26,7 @@ export const RootWrapper = ({ children }) => {
         src="https://static.hotjar.com/c/hotjar-3170670.js?sv=6"
         strategy="off-main-thread"
         forward={[`hj`, `_hjSettings`]}
-      ></Script>
+      ></Script> */}
       {children}
     </>
   );
